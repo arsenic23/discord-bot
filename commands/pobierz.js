84cb77020415350e2aa35375acc933033b35a934
arsenic23 <113555529+arsenic23@users.noreply.github.com>
2024-02-27 00:17:05 +0100

    'App Store' word variety fix

--- commands/pobierz.js
+++ commands/pobierz.js
@@ -6,7 +6,7 @@ module.exports = {
         .setDescription('Link do pobrania Vulcanovy na IOS'),
     async execute(interaction, client) {
             const embed = new EmbedBuilder()
-            .setTitle('Vulcanova na AppStore')
+            .setTitle('Vulcanova na App Store')
             .setDescription('Aplikacja Vulcanova jest już dostępna na App Store i możesz ją pobrać klikając w ten link: [App Store](https://apps.apple.com/pl/app/vulcanova/id6451334441)')
             .setColor('#0c79c7')
             .addFields({ name: 'Vulcanova Beta', value: 'Jeśli chcesz uczestniczyć w testowaniu aplikacji Vulcanova na platformie Testflight to skorzystaj z tego linku: [TestFlight](https://testflight.apple.com/join/fGMjqr58)'})
