7607417439ef38030f3b0f31f14a18e0130b4945
Wiktor Stawicki <105926196+rotkiW285@users.noreply.github.com>
2024-04-01 21:12:38 +0200

    Fix changes requested by @v0idzz
    Oprócz tej poprawki
    
    commands/android.js
    Comment on lines +19 to +23
    
    "Kod odpowiedzialny za konstruowanie wiadomości w wielu miejscach się powtarza. Chodzi głównie o wywołania setTimestamp(), setFooter(), setColor(). Być może warto wydzielić jakąś wspólną funkcję pomocniczą, by uniknąć powtórzeń."

--- commands/pobierz.js
+++ commands/pobierz.js
@@ -3,18 +3,18 @@ const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("pobierz")
-    .setDescription("Link do pobrania Vulcanovej na IOS"),
+    .setDescription("Link do pobrania Vulcanovej na iOS"),
   async execute(interaction, client) {
     const embed = new EmbedBuilder()
-      .setTitle("Vulcanova na AppStore")
+      .setTitle("Vulcanova na App Store")
       .setDescription(
-        "Aplikacja Vulcanova jest już dostępna na App Store i możesz ją pobrać klikając w ten link: [App Store](https://apps.apple.com/pl/app/vulcanova/id6451334441)",
+        "[Pobierz aplikację Vulcanova z App Store](https://apps.apple.com/pl/app/vulcanova/id6451334441)",
       )
       .setColor("#0c79c7")
       .addFields({
         name: "Vulcanova Beta",
         value:
-          "Jeśli chcesz uczestniczyć w testowaniu aplikacji Vulcanova na platformie Testflight to skorzystaj z tego linku: [TestFlight](https://testflight.apple.com/join/fGMjqr58)",
+          "Lubisz życie na krawędzi? Dołącz do beta testów aplikacji na platformie [TestFlight](https://testflight.apple.com/join/fGMjqr58)",
       })
       .setTimestamp()
       .setFooter({
